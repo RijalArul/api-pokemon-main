61c945fcbf0186e5c87d7c2cdda045e9d45c56c2
RijalArul <fakhrulmrijal7599@gmail.com>
2022-03-03 07:06:02 +0700

    Pokemon Bookmark 2

--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -66,6 +66,21 @@ class BookmarkController {
       })
     }
   }
+
+  static async all (req, res) {
+    try {
+      const response = await API_BOOKMARK_SERVICE.get(`bookmarks`)
+
+      const { data } = response.data
+
+      res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json({
+        message: 'Internal Server Error',
+        status: false
+      })
+    }
+  }
 }
 
 module.exports = BookmarkController
--- routes/bookmark.js
+++ routes/bookmark.js
@@ -5,4 +5,5 @@ const router = require('express').Router()
 router.post('/catch/:id', BookmarkController.catch)
 router.delete('/release/:id', BookmarkController.release)
 router.put('/rename/:id', BookmarkController.rename)
+router.get('/', BookmarkController.all)
 module.exports = router
